dee6a2dca6d595d42964b40bd2240e5eb73a4020
Betaray-Bill <suryaorion12@gmail.com>
2025-02-26 18:48:46 +0530

    basic routes

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -1,20 +1,24 @@
 import {useState} from 'react'
 import './App.css'
-import { Route, Routes } from 'react-router'
+import {Route, Routes} from 'react-router'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import ProtectedRoutes from './utils/ProtectedRoutes.jsx'
+import Create from './Pages/Create.jsx'
 
 function App() {
 
     return (
         <div>
             <Routes>
-                <Route element={<ProtectedRoutes /> }>
-                    <Route index element={< Home />}/>
+                <Route element={< ProtectedRoutes />}>
+                    <Route path='home'>
+                        <Route index element={< Home />} />
+                        <Route path='create' element={< Create />}/>
+                    </Route>
                 </Route>
-                
-                <Route path="login" element={<Login />}/>
+
+                <Route path="login" element={< Login />}/>
             </Routes>
 
         </div>
