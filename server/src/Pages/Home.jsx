fd7fd5aa950116b90cb4b202598eb37d09ffa1f9
Abhinandhan S <abhi.varadan@gmail.com>
2025-02-26 20:09:42 +0530

    added shad cn UI pack

--- server/src/Pages/Home.jsx
+++ server/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react' 
 import Nav from '../Layout/Components/Nav'
+import { Button } from "@/components/ui/button"
 
 function Home() {
   return (
     <div className=''>
       <Nav />
+      <Button >hello</Button>
     </div>
   )
 }
