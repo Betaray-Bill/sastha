2baf05410bf008634dbf311bccb27c53c0a237b7
Betaray-Bill <suryaorion12@gmail.com>
2025-03-14 20:37:28 +0530

    Table specs

--- server/src/Layout/Create/Dashboard/TableSpecs.jsx
+++ server/src/Layout/Create/Dashboard/TableSpecs.jsx
@@ -4,16 +4,19 @@ import {Card, CardContent} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import {GensetSpecifications} from '../../../utils/constants';
 import {toast} from "sonner";
-import { Input } from "@/components/ui/input"
-import { structureValueConstruction } from '../../../utils/functions';
+import {Input} from "@/components/ui/input"
+import {structureValueConstruction} from '../../../utils/functions';
 
 function TableSpecs() {
     const [selectedItems,
         setSelectedItems] = useState([]);
 
-    const [tableData, setTableData] = useState([]);
-    const [modelNumber, setModelNumber] = useState(0);
-    const [model, setModel] = useState([]);
+    const [tableData,
+        setTableData] = useState([]);
+    const [modelNumber,
+        setModelNumber] = useState(0);
+    const [model,
+        setModel] = useState([]);
 
     const handleSelect = (value) => {
         if (!selectedItems.includes(value)) {
@@ -22,48 +25,204 @@ function TableSpecs() {
                 value
             ]);
 
-            if(value === GensetSpecifications.PRIME_RATING_AT_RATED_RPM){
+            if (value === GensetSpecifications.PRIME_RATING_AT_RATED_RPM) {
                 const structure = {
-                    name : value,
-                    units:["KVa", "KW"],
-                    type:[],
+                    name: value,
+                    units: [
+                        "KVa", "KW"
+                    ],
+                    type: []
                 }
                 structure.value = structureValueConstruction(structure, modelNumber)
-                setTableData([...tableData, structure]);
+                setTableData(p =>[
+                    ...p,
+                    structure
+                ]);
             }
 
-            if(value === GensetSpecifications.OVERALL_DIMENSIONS_OF_GENSET){
+            if (value === GensetSpecifications.OVERALL_DIMENSIONS_OF_GENSET) {
                 const structure = {
-                    name : value,
-                    units:["mm"],
-                    type:["Length", "Width", "Height"]
+                    name: value,
+                    units: ["mm"],
+                    type: ["Length", "Width", "Height"]
                 }
                 structure.value = structureValueConstruction(structure, modelNumber)
-                setTableData([...tableData, structure]);
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
             }
 
-            if(value === GensetSpecifications.FREQUENCY){
+            if (value === GensetSpecifications.FUEL_CONSUMPTION) {
                 const structure = {
-                    name : value,
-                    units:["Hz"],
+                    name: value,
+                    units: ["Ltrs/Hr"],
+                    type: ["At 100% Load","At 75% Load","At 50% Load"]
                 }
-                structure.value = 50
-                setTableData([...tableData, structure]);
+                structure.value = structureValueConstruction(structure, modelNumber)
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
             }
 
-            if(value === GensetSpecifications.POWER_FACTOR){
+            if (value === GensetSpecifications.FREQUENCY) {
                 const structure = {
-                    name : value,
-                    units:["Lagging"],
+                    name: value,
+                    units: ["Hz"]
                 }
                 structure.value = 50
-                setTableData([...tableData, structure]);
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.POWER_FACTOR) {
+                const structure = {
+                    name: value,
+                    units: ["Lagging"]
+                }
+                structure.value = 50
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.VOLTAGE) {
+                const structure = {
+                    name: value,
+                    units: ["V"],
+                    value:''
+                }
+                // structure.value = 50
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.ELECTRICAL_BATTERY_STARTING_VOLTAGE) {
+                const structure = {
+                    name: value,
+                    units: ["Volts-DC"],
+                    value:[]
+                }
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.WEIGHT_OF_GENSET_WITH_CANOPY_APPROX) {
+                const structure = {
+                    name: value,
+                    units: ["Kg"],
+                    type: ["Dry"],
+                    value:''
+                }
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.DG_SET_NOISE_LEVEL_AT_1MTR_WITH_GENSET_CANOPY_DBA_CONTACT) {
+                const structure = {
+                    name: value,
+                    units: ["Kg"],
+                    type: ["Dry"],
+                    value:''
+                }
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.FUEL_TANK_CAPACITY) {
+                const structure = {
+                    name: value,
+                    units: ["Ltrs"],
+                    value:''
+                }
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.GOVERNING_CLASS) {
+                const structure = {
+                    name: value,
+                    units: [""],
+                    value:''
+                }
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
+            }
+
+            if (value === GensetSpecifications.ELECTRICAL_BATTERY_STARTING_VOLTAGE) {
+                const structure = {
+                    name: value,
+                    units: ["Volts-DC"],
+                    value:''
+                }
+                setTableData([
+                    ...tableData,
+                    structure
+                ]);
             }
         } else {
             toast.success("Already Added");
         }
     };
-    console.log(model)
+    console.log(tableData)
+
+
+    const handleChange = (val, inputIndex, unitsIndex, key, name) => {
+        console.log(val, inputIndex, unitsIndex, key, name) 
+        if(key === "units"){
+            let u = tableData?.map(e => {
+                if (e.name === name) {
+                    return {
+                        ...e,
+                        value:e.value?.map((vals, index) =>
+                            index === unitsIndex
+                                ? vals.map((v, i) => (i === inputIndex ? val : v)) // Modify the inner array properly
+                                : vals
+                        )
+                    };
+                }
+                return e;
+            });
+            
+            console.log(u);
+            setTableData(u);
+        }else{
+            let u = tableData?.map(e => {
+                if (e.name === name) {
+                    return {
+                        ...e,
+                        value:e.value?.map((vals, index) =>
+                            index === unitsIndex
+                                ? vals.map((v, i) => (i === inputIndex ? val : v)) // Modify the inner array properly
+                                : vals
+                        )
+                    };
+                }
+                return e;
+            });
+            
+            console.log(u);
+            setTableData(u);
+        }
+    }
+
+    console.log(tableData)
     return (
         <div className="w-[80vw] mx-auto mt-6">
             <Card className="p-4">
@@ -73,22 +232,29 @@ function TableSpecs() {
                 <div className='border p-4 rounded-md'>
                     <div className='flex items-center'>
                         <label>Number of Generators</label>
-                        <Input type="number"   onChange={e => setModelNumber(e.target.value)} className="w-20 ml-5" />
+                        <Input
+                            type="number"
+                            onChange={e => setModelNumber(e.target.value)}
+                            className="w-20 ml-5"/>
                     </div>
-                    
+
                     <div className='grid grid-cols-3 gap-4 my-6'>
-                        {
-                            modelNumber > 0 && Array.from({ length: modelNumber }).map((e, _i) => (
+                        {modelNumber > 0 && Array
+                            .from({length: modelNumber})
+                            .map((e, _i) => (
                                 <div className='flex items-center'>
-                                    <label>Enter Model Number {_i+1}</label>
-                                    <Input type="text" onChange={e => setModel(p => {
+                                    <label>Enter Model Number {_i + 1}</label>
+                                    <Input
+                                        type="text"
+                                        onChange={e => setModel(p => {
                                         const u = [...p];
                                         u[_i] = e.target.value;
                                         return u;
-                                    })} className="w-[150px] ml-5" />
+                                    })}
+                                        className="w-[150px] ml-5"/>
                                 </div>
                             ))
-                        }
+}
                     </div>
                 </div>
                 <div className="space-y-4">
@@ -110,33 +276,60 @@ function TableSpecs() {
 
                 <div className="mt-6">
                     <h2 className="text-lg font-semibold">Current Table Data</h2>
-                    <table className="w-full border-collapse  border-2 mt-2">
-                    <thead>
-                        <tr>
-                        {/* <th className="border p-2">Name</th>
-                        <th className="border p-2">Units</th>
-                        <th className="border p-2">Type</th> */}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tableData.map((structure, index) =>
-                        structure.units.map((unit, unitIndex) => (
-                            <tr key={`${index}-${unitIndex}`}>
-                                {/* Only apply rowSpan to the first unit row */}
-                                
-                                {unitIndex === 0 && (
-                                    <td className="border p-2" rowSpan={structure.units.length}>
-                                    {structure.name}
-                                    </td>
-                                )}
-                                
-                                <td className="border p-2">{unit}</td>
-                                <td className="border p-2">{structure.type[unitIndex] || "-"}</td>
-                            </tr>
-                        ))
-                        )}
-                    </tbody>
-                    </table>
+
+                    {tableData?.map((structure, index) => <div key={`${index}-${index}`} className='mt-2 border px-3 py-2 rounded-md'>
+                        {/* Only apply rowSpan to the first unit row */}
+                        <div className="font-semibold text-md">
+                            {index + 1}. {structure.name}
+                        </div>
+                        {structure.type.length == 0
+                            ? <div className='flex flex-col'>
+                                    
+                                    {structure
+                                        .units
+                                        .map((e, _i) => (
+                                            <div key={_i} className='flex items-center'>
+                                                <label className=''>{structure.units[_i]}</label>
+                                                {
+                                                    Array.from({length:modelNumber}).map((v, __i) => (
+                                                        <Input 
+                                                            className="w-max m-2"
+                                                            onChange={(val) => {
+                                                                handleChange(val.target.value, __i, _i, "units", structure.name)
+                                                            }}
+                                                        />
+                                                    ))
+                                                }
+                                            </div>
+                                        ))
+}
+                                </div>
+                            : <div className='p-3'>
+                                {structure
+                                        .type
+                                        .map((e, _i) => (
+                                            <div key={_i} className='flex items-center'>
+                                                <label className=''>{structure.type[_i]}</label>
+                                                <label className='ml-3'>({structure.units[0]})</label>
+                                                {
+                                                    Array.from({length:modelNumber}).map((v, __i) => (
+                                                        <Input 
+                                                            className="w-max m-2"
+                                                            onChange={(val) => {
+                                                                handleChange(val.target.value, __i, _i, "type", structure.name)
+                                                                
+                                                            }}
+                                                        />
+                                                    ))
+                                                }
+                                            </div>
+                                        ))
+}
+                            </div>
+}
+                        {/* <div className="">{unit}</div>
+                            <div className="">{structure.type[unitIndex] || "-"}</div> */}
+                    </div>)}
                 </div>
             </Card>
 
